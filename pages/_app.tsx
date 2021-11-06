e39bece479387e49be68ada9d2b3bf0f7205f249
Yuri Rykov <rykov87yuri@gmail.com>
2021-11-06 15:20:59 +0300

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import { EmotionCache } from '@emotion/utils';
+import { createEmotionCache, theme } from 'material';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
-export default MyApp
+export type MyAppProps = AppProps & {
+    emotionCache: EmotionCache;
+};
+
+const MyApp: React.FC<MyAppProps> = (props) => {
+    const {
+        Component,
+        emotionCache = clientSideEmotionCache,
+        pageProps,
+    } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>SMM-специалист Александра</title>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1, width=device-width"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
+};
+
+export default MyApp;
